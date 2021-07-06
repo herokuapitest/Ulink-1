eff228fa980fc4fe1da5f0d0b7ddb79a352a3653
herokuapitest <86901775+herokuapitest@users.noreply.github.com>
2021-07-06 16:58:14 +0530

    Update title.js

--- js/title.js
+++ js/title.js
@@ -3,7 +3,7 @@ var xmlhttp = new XMLHttpRequest();
       xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var Link = JSON.parse(this.responseText);
-          document.title = Link.name + " |   Ulink"
+          document.title = Link.name
 
 
         }
